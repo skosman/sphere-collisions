d1cf6e5a7e1008bef250aece31c164f518e3b3d2
Samantha Kosman <skosman@uvic.ca>
2022-08-12 21:48:43 -0700

    Define the interface for the sphere object in the header file. This includes the constructor and destructor of the sphere along data members like the radius, mass, position vector and velocity which will be used to draw and move the sphere.

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -1,13 +1,39 @@
 #ifndef SP_SPHERE_HPP
 #define SP_SPHERE_HPP
 
-// namespace sp {
-//   class sphere {
-//     public:
+#include <vector>
 
-//     private:
-//   }
-// }
+class sphere {
+  public:
+    // Sphere constructor
+    sphere(float radius, float mass, std::vector<float> position, std::vector<float> velocity);
 
+    // Sphere destructor
+    ~sphere();
+
+    // Returns the radius of the sphere
+    float get_radius() const;
+    // Returns the mass of the sphere
+    float get_mass() const;
+
+    // Get the x, y, and z position coordinates of the sphere
+    std::vector<float> get_position() const;
+
+    // Returns the x, y, and z velocity coordinates of the sphere
+    std::vector<float> get_velocity() const;
+
+
+  private:
+    // Radius of the sphere
+    float radius_;
+    // Mass of the sphere
+    float mass_;
+
+    // Vector containing the x, y, and z coordinate positions
+    std::vector<float> position_;
+
+    // Vector containing the x, y, and z coordinate velocity
+    std::vector<float> velocity_;
+};
 
 #endif
