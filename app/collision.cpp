adc462dc67246b779c5f622763338db0f0a9876c
Samantha Kosman <skosman@uvic.ca>
2022-08-12 23:14:16 -0700

    Add initial file for handling the collisions in the project. The two types of collisions are between two spheres or a sphere and a wall. Therefore, two functions are created to handle each case.

---
+++ app/collision.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <sphere.hpp>
+
+// Code for handling collision between two spheres
+// or a sphere and a wall
+
+
+// Handles collision between two spheres
+// Precondition: The two given spheres are not the same sphere
+void sphere_collision(sphere s1, sphere s2) {
+
+}
+
+// Handles collision between the wall and a sphere
+void wall_collision(sphere s) {
+
+}
