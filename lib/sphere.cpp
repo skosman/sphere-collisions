e8b7e06c176bf92116399341b3832362d6962334
Samantha Kosman <skosman@uvic.ca>
2022-08-12 21:58:37 -0700

    Implement data member functions for sphere class. Also remove unnecessary destructor. The data member functions include the constructor along with the getters for the data members.

--- lib/sphere.cpp
+++ lib/sphere.cpp
@@ -1,1 +1,30 @@
 #include <sphere.hpp>
+
+#include <vector>
+
+  // Sphere constructor
+  sphere::sphere(float radius, float mass, std::vector<float> position, std::vector<float> velocity) {
+    radius_ = radius;
+    mass_ = mass_;
+    position_ = position;
+    velocity_ = velocity;
+  }
+
+  // Returns the radius of the sphere
+  float sphere::get_radius() const {
+    return radius_;
+  }
+  // Returns the mass of the sphere
+  float sphere::get_mass() const {
+    return mass_;
+  }
+
+  // Get the x, y, and z position coordinates of the sphere
+  std::vector<float> sphere::get_position() const {
+    return position_;
+  }
+
+  // Returns the x, y, and z velocity coordinates of the sphere
+  std::vector<float> sphere::get_velocity() const {
+    return velocity_;
+  }
