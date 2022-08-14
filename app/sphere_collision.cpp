c969fb34ca09e771aa49db4376b46ce4e7466e9b
Samantha Kosman <skosman@uvic.ca>
2022-08-14 15:22:25 -0700

    Change to use GLUT to handle openGL operations due to error using glvectex3f not rendering sphere's properly. Set initial properties for creating the openGL window with GLUT, the openGL utility toolkit.

--- app/sphere_collision.cpp
+++ app/sphere_collision.cpp
@@ -1,67 +1,24 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h> // GLFW Helper library
-#include <iostream>
+#ifdef __APPLE_CC__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 
 
-// Initial setup for program using https://antongerdelan.net/opengl/hellotriangle.html
-// as a reference 
-
-// TODO: Add initial sphere objects to render
-
-// Callback function for keyboard input
-// TODO: Update function to handle the up and down arrow
-// for adding and removing spheres
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    std::cout << "Keyboard input" << key;
-}
-
-int main() {
-  // Start GL context and OS window using the GLFW helper library
-  if (!glfwInit()) {
-    std::cout << "Error: Could not start GLFW\n";
-    return 1;
-  }
-
-  // For macOs
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  // Create 640x480 gflw window
-  GLFWwindow *window = glfwCreateWindow(640, 480, "Sphere Collision", NULL, NULL);
-  if (!window) {
-    std::cout << "Error: Could not open window with GLFW\n";
-    glfwTerminate();
-    return 1;
-  }
+// Main program that runs the sphere collision program
+int main(int argc, char *argv[]) {
+  // Initialize GLUT to create an openGL window
+  glutInit(&argc, argv);
+  // Create a 600 by 600 window
+  int WINDOW = 600;
+  glutInitWindowSize(WINDOW, WINDOW);
+  // Position the window in the top left side of the screen
+  glutInitWindowPosition(WINDOW / 4, WINDOW / 4);
+  // Set the display mode to RGB color, double buffer for smooth animation, adn depth buffer
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+  // Create window with given title
+  glutCreateWindow("Sphere Collision");
 
 
-  // Give window OpenGL context
-  glfwMakeContextCurrent(window);
-  // Tell GL to capture keyboard input
-  glfwSetKeyCallback(window, key_callback);
-
-  // start GLEW extension handler
-  glewExperimental = GL_TRUE;
-  glewInit();
-
-  // Tell GL to only draw onto a pixel if the shape is closer to the viewer
-  glEnable(GL_DEPTH_TEST); // enable depth-testing
-  glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
-
-  // Loop that renders items
-  while(!glfwWindowShouldClose(window)) {
-    // wipe the drawing surface clear
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // update other events like input handling 
-    glfwPollEvents();
-    // put the stuff we've been drawing onto the display
-    glfwSwapBuffers(window);
-  }
-
-  // close GL context and any other GLFW resources
-  glfwTerminate();
   return 0;
-
 }
